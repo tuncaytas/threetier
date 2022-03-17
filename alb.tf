cb409794bcb0e4f76ef47e0b13239f4cfcf5ce8f
tuncaytas <tnc.tas@gmail.com>
2022-03-17 20:06:17 +0000

    ecs fargate changed

--- alb.tf
+++ alb.tf
@@ -1,15 +1,10 @@
 module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
-
-  name = "threetier"
-
+  source             = "terraform-aws-modules/alb/aws"
+  name               = "threetier"
   load_balancer_type = "application"
-
-  vpc_id          = "vpc-063d33c2607f516b9"
-  subnets         = ["subnet-0b5d844078b6befec", "subnet-046be52b4e01cc29a", "subnet-082ab3abe05b94a65"]
-  security_groups = ["sg-02315e4a266c2cefb"]
-
+  vpc_id             = "vpc-063d33c2607f516b9"
+  subnets            = ["subnet-0b5d844078b6befec", "subnet-046be52b4e01cc29a", "subnet-082ab3abe05b94a65"]
+  security_groups    = ["sg-02315e4a266c2cefb"]
   target_groups = [
     {
       name_prefix      = "pref-"
@@ -26,15 +21,6 @@ module "alb" {
           port      = 8080
         }
       ]
-    }
-  ]
-
-  https_listeners = [
-    {
-      port               = 443
-      protocol           = "HTTPS"
-      certificate_arn    = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-      target_group_index = 0
     }
   ]
 
--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  // required_version = "0.14.11"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.42.0"
+    }
+  }
+}
 provider "aws" {
   region = "us-east-2"
 }
