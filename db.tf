9b7740347ffee136eba571aa8103bb775479fba2
farrukh90 <farrukhsadykov@gmail.com>
2022-03-17 17:18:47 -0500

    added subnets

--- db.tf
+++ db.tf
@@ -1,35 +1,35 @@
-module "db" {
-  source = "terraform-aws-modules/rds/aws"
+// module "db" {
+//   source = "terraform-aws-modules/rds/aws"
 
-  identifier = "demodb"
+//   identifier = "demodb"
 
-  engine            = "mysql"
-  engine_version    = "5.7.25"
-  instance_class    = "db.t3a.large"
-  allocated_storage = 5
-  db_name  = "threetier"
-  username = "user"
-  port     = "3306"
-  vpc_security_group_ids = ["vpc-063d33c2607f516b9"]
+//   engine            = "mysql"
+//   engine_version    = "5.7.25"
+//   instance_class    = "db.t3a.large"
+//   allocated_storage = 5
+//   db_name  = "threetier"
+//   username = "user"
+//   port     = "3306"
+//   vpc_security_group_ids = ["vpc-063d33c2607f516b9"]
 
-  // maintenance_window = "Mon:00:00-Mon:03:00"
-  // backup_window      = "03:00-06:00"
+//   // maintenance_window = "Mon:00:00-Mon:03:00"
+//   // backup_window      = "03:00-06:00"
 
-  # Enhanced Monitoring - see example for details on how to create the role
-  # by yourself, in case you don't want to create it automatically
-  // monitoring_interval    = "30"
-  // monitoring_role_name   = "MyRDSMonitoringRole"
-  // create_monitoring_role = true
+//   # Enhanced Monitoring - see example for details on how to create the role
+//   # by yourself, in case you don't want to create it automatically
+//   // monitoring_interval    = "30"
+//   // monitoring_role_name   = "MyRDSMonitoringRole"
+//   // create_monitoring_role = true
 
-}
-// resource "aws_rds_cluster" "threetier" {
-//   cluster_identifier      = "rds-cluster-threetier"
-//   engine                  = "rds"
-//   engine_version          = "5.7.25"
-//   availability_zones      = ["us-east-2a", "us-east-2b", "us-east-2c"]
-//   database_name           = "threetier"
-//   master_username         = "admin"
-//   master_password         = "bar"
-//   backup_retention_period = 5
-// //   preferred_backup_window = "07:00-09:00"
 // }
+// // resource "aws_rds_cluster" "threetier" {
+// //   cluster_identifier      = "rds-cluster-threetier"
+// //   engine                  = "rds"
+// //   engine_version          = "5.7.25"
+// //   availability_zones      = ["us-east-2a", "us-east-2b", "us-east-2c"]
+// //   database_name           = "threetier"
+// //   master_username         = "admin"
+// //   master_password         = "bar"
+// //   backup_retention_period = 5
+// // //   preferred_backup_window = "07:00-09:00"
+// // }
--- provider.tf
+++ provider.tf
@@ -1,4 +1,3 @@
 provider "aws" {
-  region = "us-east-2"
+    region = var.region
 }
-
