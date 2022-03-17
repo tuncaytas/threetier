0994434088946f8d68e6003e2b8549a8372ace51
tuncaytas <tnc.tas@gmail.com>
2022-03-17 20:43:06 +0000

    provider

--- provider.tf
+++ provider.tf
@@ -1,12 +1,4 @@
-terraform {
-  // required_version = "0.14.11"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 2.42.0"
-    }
-  }
-}
+
 provider "aws" {
   region = "us-east-2"
 }
