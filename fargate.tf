23836a9ae949fb846d1483feaae773ecdb5bf43e
tuncaytas <tnc.tas@gmail.com>
2022-03-18 23:09:26 +0000

    edit task definition and env var

--- fargate.tf
+++ fargate.tf
@@ -1,29 +1,81 @@
+// resource "aws_ecs_task_definition" "wordpress" {
+//   family                   = "test"
+//   requires_compatibilities = ["FARGATE"]
+//   network_mode             = "awsvpc"
+//   cpu                      = 1024
+//   memory                   = 2048
+//   container_definitions    = <<TASK_DEFINITION
+// [
+//   {
+//     "name": "wordpress",
+//     "image": "docker.io/wordpress:latest",
+//     "cpu": 1024,
+//     "memory": 2048,
+//     "essential": true,
+//     "portMappings": [
+//       {
+//         "containerPort": 80,
+//         "hostPort": 80
+//       }
+//     ],
+//     "environment": [
+//       {"name": "WORDPRESS_DB_HOST", "value": "aws_db_instance.threetier.address"}
+//     ]
+//   }
+// ]
+// TASK_DEFINITION
+
+//   runtime_platform {
+//     operating_system_family = "LINUX"
+//     cpu_architecture        = "X86_64"
+//   }
+// }
+
 resource "aws_ecs_task_definition" "wordpress" {
-  family                   = "test"
+  family                   = "threetier"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = 1024
   memory                   = 2048
-  container_definitions    = <<TASK_DEFINITION
-[
-  {
-    "name": "wordpress",
-    "image": "docker.io/wordpress:latest",
-    "cpu": 1024,
-    "memory": 2048,
-    "essential": true,
-    "portMappings": [
-      {
-        "containerPort": 80,
-        "hostPort": 80
-      }
-    ],
-    "environment": [
-      {"name": "WORDPRESS_DB_HOST", "value": "aws_db_instance.threetier.address"}
-    ]
-  }
-]
-TASK_DEFINITION
+  container_definitions = jsonencode([
+    {
+      name      = "wordpress"
+      image     = "docker.io/wordpress:latest"
+      cpu       = 1024
+      memory    = 2048
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ]
+      environment = [
+        {
+          name = "WORDPRESS_DB_HOST"
+          value = "${aws_db_instance.threetier.address}"
+        },
+        {
+          name= "WORDPRESS_DB_USER"
+          value = "admin"
+        },
+        {
+          name= "WORDPRESS_DB_PASSWORD" 
+          value= "bar"
+        },
+        {
+          name = "WORDPRESS_DB_NAME"
+          value = "wordpress"
+        },
+        {
+          name = "WORDPRESS_DB_PORT" 
+          value = "3306"
+        }
+      ]
+    }
+  ])
 
   runtime_platform {
     operating_system_family = "LINUX"
